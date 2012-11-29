2c7593b383eca8a5602643552f271c590fec6f07
Samuel <samuel@Dinner.(none)>
2012-11-28 22:08:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArcadeFight::Application.config.secret_token = 'd6ba951ebb30cc626367c5ce59ade16731bb0aebfd830b348def797d194111c4da6a8a5bd0c72d1c62cc365569ba3caad0bf1e4735b23bb6cd1f69f7d21b45b9'
